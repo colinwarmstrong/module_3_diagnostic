c71011ea0c1d39d9ad9374b16f821b6b9ecac22c
Colin Armstrong <colinwarmstrong@gmail.com>
2018-08-29 09:31:11 -0600

    Adds search index and view.

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -26,7 +26,7 @@ feature "User can visit root page" do
     within(first('.station')) do
       expect(page).to have_content('Station Name: 1800 Larimer')
       expect(page).to have_content('Address: 1800 Larimer Street, Suite 1800')
-      expect(page).to have_content('Fuel Type: Electric')
+      expect(page).to have_content('Fuel Type: ELEC')
       expect(page).to have_content('Distance: 0.14269 miles')
       expect(page).to have_content('Access Times: MO: 12:00am-12:00am; TU: 12:00am-12:00am; WE: 12:00am-12:00am; TH: 12:00am-12:00am; FR: 12:00am-12:00am; SA: 12:00am-12:00am; SU: 12:00am-12:00am')
     end
